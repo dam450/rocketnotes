368ca8b4b614bd37e8af3b556a38d0181c9cf2df
Dam450 <68165752+dam450@users.noreply.github.com>
2023-01-26 23:31:01 -0300

    create Button component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,18 @@
 import { Container } from './styles'
 
+import { Button } from '../../components/Button'
+
 export function Details() {
   return (
     <Container>
       <h1>Rocketnotes</h1>
+      <p>Anote seus ideias.</p>
+      <Button title='Entrar'/>
+      <Button title='Cadastrar'/>
+      <Button title='Voltar'/>
+
     </Container>
   )
 }
-
-
 
 //export default () => (<>Rocketnotes</>)
