789ce6bf15350c79aaf2ea061b48ef0b0c640e4d
Dam450 <68165752+dam450@users.noreply.github.com>
2023-01-30 22:09:07 -0300

    create links

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,19 @@
-import { Container } from './styles'
+import { Container, Links } from './styles'
 
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
 
 export function Details() {
   return (
     <Container>
       <Header />
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+        </Links>
+      </Section>
       <Button title="Voltar" />
     </Container>
   )
