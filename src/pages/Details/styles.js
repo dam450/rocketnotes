0f0fcaa88801d26eba52f2212d476eef03554a21
Dam450 <68165752+dam450@users.noreply.github.com>
2023-01-31 11:59:29 -0300

    add details content

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -10,6 +10,12 @@ export const Container = styled.div`
   grid-template-areas:
     'header'
     'content';
+
+  > main {
+    grid-area: content;
+    overflow-y: auto;
+    padding-top: 64px;
+  }
 `
 
 export const Links = styled.ul`
@@ -25,5 +31,39 @@ export const Links = styled.ul`
         color: ${({ theme }) => theme.COLORS.GRAY_100};
       }
     }
+  }
+`
+
+export const Content = styled.div`
+  max-width: 550px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+
+  > button:first-child {
+    align-self: end;
+  }
+
+  > h1 {
+    font-weight: 500;
+    font-size: 36px;
+    line-height: 47px;
+    margin-bottom: 16px;
+    padding-top: 64px;
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+
+  > p {
+    font-size: 16px;
+    font-family: Roboto, sans-serif;
+    line-height: 19px;
+    text-align: justify;
+    margin-bottom: 8px;
+  }
+
+  > button:last-child {
+    margin: 120px 0 88px;
   }
 `
