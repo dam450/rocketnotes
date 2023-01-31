daec913e565b5b173a4537b5db07b0c911485a06
Dam450 <68165752+dam450@users.noreply.github.com>
2023-01-31 12:36:45 -0300

    add grid template to home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,13 +1,44 @@
 import styled from "styled-components";
+import theme from "../../styles/theme";
 
-export const Container = styled.div``
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
 
-export const Brand = styled.div``
+  display: grid;
 
-export const Menu = styled.ul``
+  grid-template-columns: 250px auto;
+  grid-template-rows: 105px 128px auto 64px;
+  grid-template-areas:
+    "brand header"
+    "menu search"
+    "menu content"
+    "newnote content";
 
-export const Search = styled.div``
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+`
 
-export const Content = styled.div``
+export const Brand = styled.div`
+  grid-area: brand;
+  background-color: red;
+`
 
-export const NewNote = styled.button``
+export const Menu = styled.ul`
+  grid-area: menu;
+  background-color: green;
+`
+
+export const Search = styled.div`
+  grid-area: search;
+  background-color: blue;
+`
+
+export const Content = styled.div`
+  grid-area: content;
+  background-color: yellow;
+`
+
+export const NewNote = styled.button`
+  grid-area: newnote;
+  background-color: purple;
+`
