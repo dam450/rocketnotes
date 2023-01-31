383a2508fe2ea4b5f60819cec49c8fa0947d63bf
Dam450 <68165752+dam450@users.noreply.github.com>
2023-01-30 22:08:21 -0300

    create section

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,5 +1,10 @@
 import { Container } from './styles'
 
-export function Section() {
-  return <Container></Container>
+export function Section({ title, children }) {
+  return (
+    <Container>
+      <h2>{title}</h2>
+      {children}
+    </Container>
+  )
 }
--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -1,5 +1,17 @@
 import styled from 'styled-components'
 
 export const Container = styled.section`
-  //
+  margin: 56px 0 28px;
+
+  > h2 {
+    border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+    padding-bottom: 16px;
+    margin-bottom: 16px;
+
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+
+    font-size: 20px;
+    font-weight: 400;
+  }
 `
