ae511f94483204f69110a3cd6e1bb0953e37f3d3
Dam450 <68165752+dam450@users.noreply.github.com>
2023-02-01 16:53:11 -0300

    update SignIn Page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,23 @@
-import { Container } from './styles'
+import { FiMail, FiLock } from 'react-icons/fi'
+
+import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
+
+import { Container, Form, Background } from './styles'
 
 export function SignIn() {
   return (
     <Container>
-      
+      <Form>
+        <h1>Rocketnotes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+        <h2>Faça seu login</h2>
+        <Input placeholder="E-mail" icon={FiMail} />
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Button title="Entrar" />
+        <a href='#'>Criar conta </a>
+      </Form>
+      <Background />
     </Container>
   )
 }
