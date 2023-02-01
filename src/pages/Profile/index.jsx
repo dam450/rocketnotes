1c3a4462c24e9f02a32a08ef49ca81244876c2ea
Dam450 <68165752+dam450@users.noreply.github.com>
2023-02-01 20:59:37 -0300

    create page profile

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,55 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
+
+import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
+
+import { Avatar, Container, Form } from './styles'
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="http://">
+          <FiArrowLeft size={24} />{' '}
+        </a>
+      </header>
+
+      <Form>
+        <Avatar>
+          <img src="./img/avatar.svg" alt="foto do usuário" />
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input type="file" id="avatar" accept="image/png, image/jpeg" />
+          </label>
+        </Avatar>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          autoComplete="given-name"
+        />
+        <Input
+          placeholder="E-mail"
+          type="email"
+          icon={FiMail}
+          autoComplete="email"
+        />
+
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+          autoComplete="current-password"
+        />
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+          autoComplete="new-password"
+        />
+
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  )
+}
