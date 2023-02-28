c3b82f5b0f90c4e0bee4456f460fae3ff25d0b19
Dam450 <68165752+dam450@users.noreply.github.com>
2023-02-28 20:38:02 -0300

    update details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
 import { Container, Links, Content } from './styles'
 
 import { Button } from '../../components/Button'
@@ -6,44 +9,76 @@ import { Section } from '../../components/Section'
 import { Tag } from '../../components/Tag'
 import { ButtonText } from '../../components/ButtonText'
 
+import { api } from '@/services/api'
+
 export function Details() {
+  const [data, setData] = useState(null)
+
+  const params = useParams()
+  const navigate = useNavigate()
+
+  function handleBack() {
+    navigate(-1)
+  }
+
+  async function handleDeleteNote() {
+    const confirm = window.confirm('Tem certeza que deseja deletar a nota?')
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`)
+
+      navigate('/')
+    }
+
+
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`)
+      setData(response.data)
+      console.log(response.data)
+    }
+    fetchNote()
+  }, [])
+
   return (
     <Container>
       <Header />
 
-      <main>
-        <Content>
-          <ButtonText title="Excluir a nota" />
+      {data && (
+        <main>
+          <Content>
+            <ButtonText title="Excluir a nota" onClick={() => handleDeleteNote()} />
 
-          <h1>Introdução ao React</h1>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-          <Section title="Links úteis">
-            <Links>
-              <li>
-                <a href="#">https://www.rocketseat.com.br/</a>
-              </li>
-              <li>
-                <a href="#">https://www.rocketseat.com.br/</a>
-              </li>
-            </Links>
-          </Section>
-          <Section title="Marcadores">
-            <Tag title="express" />
-            <Tag title="nodejs" />
-          </Section>
-          <Button title="Voltar" />
-        </Content>
-      </main>
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+            {data.links.length > 0 && (
+              <Section title="Links úteis">
+                <Links>
+                  {data.links.map(link => (
+                    <li key={String(link.id)}>
+                      <a href={link.url} target="_blank">
+                        {link.url}
+                      </a>
+                    </li>
+                  ))}
+                </Links>
+              </Section>
+            )}
+
+            {data.tags.length > 0 && (
+              <Section title="Marcadores">
+                {data.tags.map(tag => (
+                  <Tag key={String(tag.id)} title={tag.name} />
+                ))}
+              </Section>
+            )}
+
+            <Button title="Voltar" onClick={handleBack} />
+          </Content>
+        </main>
+      )}
     </Container>
   )
 }
