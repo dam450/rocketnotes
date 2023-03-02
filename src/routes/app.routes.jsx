31b74174202fa132fc2ac8e75bb318271b12f085
Dam450 <68165752+dam450@users.noreply.github.com>
2023-03-02 15:40:58 -0300

    redirects invalid routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,17 +1,22 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { New } from '@/pages/New'
 import { Home } from '@/pages/Home'
 import { Profile } from '@/pages/Profile'
 import { Details } from '@/pages/Details'
+import { ErrorPage } from '@/pages/ErrorPage'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/new' element={<New />} />
-      <Route path='/profile' element={<Profile />} />
-      <Route path='/details/:id' element={<Details />} />
+      <Route path="/" element={<Navigate to={'/home'} />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/details/:id" element={<Details />} />
+
+      <Route path="*" element={<Navigate to={'/notfound'} replace/>} />
+      <Route path="/notfound" element={<ErrorPage />} />
     </Routes>
   )
 }
--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,14 +1,19 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { SignIn } from '@/pages/SignIn'
 import { SignUp } from '@/pages/SignUp'
 
+
+
 export function AuthRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<SignIn />} />
-      <Route path='/register' element={<SignUp />} />
-      <Route path='*' element={<SignIn />} />
+      <Route path="/" element={<SignIn />} />
+      <Route path="/register" element={<SignUp />} />
+
+      {/* qualquer outro valor de rota vai para SignIn  */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+
     </Routes>
   )
 }
